d5b1067d84344adf19bd8461ea0e9793f13b9700
nryaskin <nik.ryaskin@gmail.com>
2017-11-19 04:52:58 -0500

    Added probability vizualization

--- lab_1/rayleigh.py
+++ lab_1/rayleigh.py
@@ -9,10 +9,21 @@ def rayleigh_probability(x, sigma):
     return (x / sigma**2) * np.exp(-x**2/(2*sigma**2))
 
 arr = rayleigh(1, 10000)
+
+plt.figure(1)
+ax1 = plt.subplot(211)
 plt.hist(arr, 50, normed=1, facecolor='g', alpha=0.75)
 plt.xlabel('Values')
 plt.ylabel('Probability')
 plt.title('Histogram of rayleigh distribution')
 plt.axis([0,6,0,1])
 plt.grid(True)
+
+ax2 = plt.subplot(212)
+
+X = np.linspace(0, 6, 200, endpoint=True)
+Y = rayleigh_probability(X, 1)
+ax2.plot(X, Y, color='blue', linewidth=1.0, linestyle='-') 
+ax2.axes.set_ybound(lower = 0, upper=1)
+ax2.axes.set_xbound(lower=0, upper=6)
 plt.show()
